d8ae2cad7d58cc406deb364bce76c188f66a794d
ElPhantasmo <56005890+ElPhantasmo@users.noreply.github.com>
2019-11-04 20:42:28 -0300

    Update tododb-dml.sql

--- scripts/tododb-dml.sql
+++ scripts/tododb-dml.sql
@@ -1,1 +1,3 @@
 INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno1', 'alumno', 'uno', 'alumno1@asd.com');
+
+INSERT INTO `tarea` (`id`, `descripcion`, `fecha_limite`, `persona_id`, `realizada`, `fecha_realizada`) VALUES (NULL, 'Crear repositorio en github', '2019-11-11', '7', '', ''); 
